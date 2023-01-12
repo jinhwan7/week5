7523c552da196a31353b29fcd3a7e728b6e22e28
jinhwan7 <rkflql851@gmail.com>
2023-01-12 13:16:26 +0900

    완성

--- services/login.service.js
+++ services/login.service.js
@@ -1,6 +1,7 @@
 const LoginRepository = require('../repositories/login.repository.js');
 const { User } = require('../models/index.js')
 const { RefToken } = require('../models/index.js')
+const bcrypt = require('bcrypt');
 
 require('dotenv').config();
 const jwt = require("jsonwebtoken");
@@ -13,26 +14,32 @@ class LoginService {
 
     login = async (nickname, password) => {
 
-        const user = await this.loginRepository.findUser(nickname, password);
-        
-        if (user.length) {//닉네임과 비번이 맞으면
-            const userId = JSON.parse(JSON.stringify(user))[0].userId;
-            console.log(userId)
+        const user = JSON.parse(JSON.stringify(await this.loginRepository.findUser(nickname)))
+        console.log(user)
+        if (!user) {
+            return {
+                status: 400,
+                message: "아이디 비밀번호가 틀렸습니다"
+            }
+        }
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (validPassword) {
+
             const accessToken = jwt.sign(
-                {userId:userId},
+                { userId: user.userId },
                 process.env.SECRET_KEY,
                 {
-                    expiresIn:'10s',
+                    expiresIn: '10s',
                 }
-            )
+            );
             const refreshToken = jwt.sign(
                 {},
                 process.env.SECRET_KEY,
                 {
-                    expiresIn:'10m'
+                    expiresIn: '10m'
                 }
-            )
-            await this.loginRepository.refTokenSave(refreshToken,userId)
+            );
+            await this.loginRepository.refTokenSave(refreshToken, user.userId)
 
             return {
                 accessToken: accessToken,
@@ -40,10 +47,11 @@ class LoginService {
                 status: 200,
                 message: "로그인성공"
             }
-        }else {
-            return{
-                status:400,
-                message:"아이디 비밀번호가 틀렸습니다"
+
+        } else {
+            return {
+                status: 400,
+                message: "아이디 비밀번호가 틀렸습니다"
             }
         }
     }
